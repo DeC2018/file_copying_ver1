e3e94dc56a4e24448bbd97feb3df105ce5b7f523
den <emailfordenis@yandex.ru>
2023-12-10 07:52:16 +1000

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+copy input to output; 1st version
+read a character
+while (charater is not end-of-file indicator)
+output the character just read
+read a character
+*/
+
+#include <stdio.h>
+
+int main(){
+    int c;
+    c = getchar();
+    while (c != EOF) {
+        putchar(c);
+        c = getchar();
+    }
+}
